de5daa232bdb67d667cc85f902ee06bb79d482f1
TMCognitic <thierry.morre@cognitic.be>
2020-07-10 15:22:08 +0200

    Ajoutez des fichiers projet.

---
+++ CurriculumVitaeCore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CurriculumVitaeCore.Models;
+
+namespace CurriculumVitaeCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {            
+            _logger.LogInformation("Appel de la page Index");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            _logger.LogWarning("Appel de la page About");
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            _logger.LogError("Appel de la page Contact");
+            return View();
+        }
+    }
+}
---
+++ CurriculumVitaeCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurriculumVitaeCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
